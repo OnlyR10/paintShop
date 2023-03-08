986723e44428ceef996a78b2b3e8e414cda6dc0d
Kirill Naydovich <kirillnaydovich@gmail.com>
2023-03-08 13:56:35 +0300

    feat: add possible style for header and footer

--- src/config/palette.js
+++ src/config/palette.js
@@ -2,6 +2,7 @@ export const MAIN_PALETTE = {
   productFirstElementBackground: "#FFFFFF",
   productSecondElementBackground: "#F6F6F6",
   edgingBackground: "#cccdd0",
+  // logoBackground: "#101649",
 };
 
 export const TEXT_PALETTE = {
