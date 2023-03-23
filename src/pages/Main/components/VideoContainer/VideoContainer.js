b7e683b233f2811349472efbe8c8ac6278974c7b
Kirill Naydovich <kirillnaydovich@gmail.com>
2023-03-23 22:59:11 +0300

    feat: add poster in video container

--- src/pages/Main/components/VideoContainer/VideoContainer.js
+++ src/pages/Main/components/VideoContainer/VideoContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import poster from "../../../../assets/images/wall.jpg";
+import poster from "../../../../assets/images/poster.jpg";
 import videoM from "../../../../assets/video/Ecoline.mp4";
 import videoW from "../../../../assets/video/Ecoline.webm";
 import { Container, VideoScreensaver, VideoSource } from "./styles";
@@ -7,7 +7,7 @@ import { Container, VideoScreensaver, VideoSource } from "./styles";
 export const VideoContainer = () => {
   return (
     <Container>
-      <VideoScreensaver autoPlay muted loop /* poster={poster} */>
+      <VideoScreensaver autoPlay muted loop poster={poster}>
         <VideoSource src={videoW} type="video/webm" />
         <VideoSource src={videoM} type="video/mp4" />
       </VideoScreensaver>
