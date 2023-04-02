d7649b18f0907042a1a98213a0b68ad53cc05e2f
Kirill Naydovich <kirillnaydovich@gmail.com>
2023-04-02 21:47:22 +0300

    feat: add breakpoints

--- src/config/breakpoints.js
+++ src/config/breakpoints.js
@@ -1,11 +1,13 @@
-export const desktopWidth = 1199;
-export const tabletWidth = 899;
-export const mobileWidth = 767;
-export const smartphoneWidth = 599;
+export const desktopWidth = 1200;
+export const laptopWidth = 1024;
+export const tabletWidth = 768;
+export const smartphoneWidth = 600;
+export const mobileWidth = 480;
 
 export const BREAKPOINTS = {
   desktop: `(max-width: ${desktopWidth}px)`,
+  laptop: `(max-width: ${laptopWidth}px)`,
   tablet: `(max-width: ${tabletWidth}px)`,
-  mobile: `(max-width: ${mobileWidth}px)`,
   smartphone: `(max-width: ${smartphoneWidth}px)`,
+  mobile: `(max-width: ${mobileWidth}px)`,
 };
