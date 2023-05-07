afe6dd66495d7372bad1b10e5afcd066662c04e7
Kirill Naydovich <kirillnaydovich@gmail.com>
2023-05-07 16:29:47 +0300

    fix: add base name for github deploy

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalStyles } from "./GlobalStyles";
 import { Router } from "./Router/Routes";
+import { BASE_NAME } from "./constants/roots";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={BASE_NAME}>
       <GlobalStyles />
       <Router />
     </BrowserRouter>
