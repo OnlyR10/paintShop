af18e58bb11e7b847d6f753ada8988ee3a6a64e5
Kirill Naydovich <kirillnaydovich@gmail.com>
2023-05-07 13:22:20 +0300

    feat: add jivo toggler

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
@@ -8,6 +8,10 @@ import { Container } from "./styles";
 
 export const Layout = () => {
   const resize = useResize();
+
+  useEffect(() => {
+    resize.smartphone ? window.jivo_destroy() : window.jivo_init();
+  }, [resize.smartphone]);
 
   return (
     <Context.Provider value={resize}>
