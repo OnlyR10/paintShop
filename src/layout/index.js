2d63d5a3ca722273849d8b5a8a437700ba3fd806
Kirill Naydovich <kirillnaydovich@gmail.com>
2023-11-13 20:07:12 +0300

    feat: remove jivo when resize display

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { Outlet } from "react-router-dom";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
@@ -8,24 +8,34 @@ import { Container } from "./styles";
 
 export const Layout = () => {
   const resize = useResize();
+  const jivoRef = useRef(null);
 
   useEffect(() => {
     const script = document.createElement("script");
-    const jivo = document.getElementById("jivo");
+    script.id = "jivo";
+    script.src = "//code.jivo.ru/widget/Kxr1QZC8QM";
+    document.body.append(script);
 
-    const loadScript = (url) => {
-      script.id = "jivo";
-      script.src = url;
-      document.body.append(script);
-    };
+    let observer = new MutationObserver(() => {
+      if (!jivoRef.current)
+        jivoRef.current = document.body.getElementsByTagName("jdiv")[0];
+    });
 
-    if (resize.smartphone && jivo) {
-      window.jivo_destroy();
-      jivo.remove();
-    } else if (!resize.smartphone && !jivo) {
-      loadScript("//code.jivo.ru/widget/Kxr1QZC8QM");
+    observer.observe(document.body, {
+      childList: true,
+    });
+  }, []);
+
+  useEffect(() => {
+    const jivo = jivoRef.current?.style;
+    if (jivo) {
+      resize.tablet ? (jivo.display = "none") : (jivo.display = "");
     }
-  }, [resize.smartphone]);
+
+    // console.dir(jivoRef.current);
+    // TODO jivo исчезает, когда происходит resize экрана
+    // TODO но не когда экран изначально маленький (тогда jivo все-равно загружается)
+  }, [resize.tablet, jivoRef.current]);
 
   return (
     <Context.Provider value={resize}>
