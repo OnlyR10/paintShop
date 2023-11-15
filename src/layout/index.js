944dd6fb59cdf230a88c64189988ab9241942daf
Kirill Naydovich <kirillnaydovich@gmail.com>
2023-11-15 21:35:57 +0300

    feat: remove jivo

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect } from "react";
+import React, { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
@@ -8,24 +8,6 @@ import { Container, FallbackContainer } from "./styles";
 
 export const Layout = () => {
   const resize = useResize();
-
-  useEffect(() => {
-    const script = document.createElement("script");
-    const jivo = document.getElementById("jivo");
-
-    const loadScript = (url) => {
-      script.id = "jivo";
-      script.src = url;
-      document.body.append(script);
-    };
-
-    if (resize.smartphone && jivo) {
-      window.jivo_destroy();
-      jivo.remove();
-    } else if (!resize.smartphone && !jivo) {
-      loadScript("//code.jivo.ru/widget/Kxr1QZC8QM");
-    }
-  }, [resize.smartphone]);
 
   return (
     <Context.Provider value={resize}>
